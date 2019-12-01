acb983498b9b60370ca9d98fb8e7b1398b13877e
iosue <joshua.j.cassell@gmail.com>
2019-12-01 18:10:19 +0000

    updated README

--- autostart.sh
+++ autostart.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 compton &
 sleep 1 &
-discord &
-#spotify &
+#discord &
 nitrogen --restore &
 bin/res 1080&
-sudo chmod 777 /sys/devices/pci0000:00/0000:00:02.0/drm/card*/card*-eDP-1/intel_backlight/brightness &
-chmod 777 /sys/devices/pci0000:00/0000:00:02.0/drm/card*/card*-eDP-1/intel_backlight/brightness &
 while true; do
 	xsetroot -name "` date +"%e, %b %Y - %R" `"
 	sleep 1m
 done &
-#dwmbar &
-#dunst &
